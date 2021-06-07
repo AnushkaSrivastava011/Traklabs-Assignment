da6ce1b2fb82b9dc5865cd193ad034eb045d648b
AnushkaSrivastava <56473698+AnushkaSrivastava011@users.noreply.github.com>
2021-06-07 22:19:29 +0530

    adding assignment

---
+++ build/routes/employeeRoutes.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var express_1 = __importDefault(require("express"));
+var employeeController_1 = __importDefault(require("../controllers/employeeController"));
+var router = express_1.default.Router();
+router.get('/', employeeController_1.default.getEmployees);
+router.get('/api/:id', employeeController_1.default.getEmployeeById);
+router.get('/name', employeeController_1.default.getEmployeeByName);
+router.get('/dept/:id', employeeController_1.default.getEmployeesOfSameDepartment);
+router.post('/', employeeController_1.default.addNewEmployee);
+router.put('/:id', employeeController_1.default.updateEmployee);
+router.delete('/:id', employeeController_1.default.deleteEmployeeById);
+module.exports = router;
